f99660d4b73ccfdb41e7352a7d54aa9e78c9705e
Andre Botta <andre.botta@outcomex.com.au>
2019-06-21 16:26:47 +1000

    feat (App.js): finish basic structure
    finish basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Select from "react-select";
+import { Form, FormGroup, Label, Input } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./scss/style.css";
+const options = [
+  { value: "feat", label: "feat" },
+  { value: "fix", label: "fix" },
+  { value: "docs", label: "docs" },
+  { value: "style", label: "style" },
+  { value: "refactor", label: "refactor" },
+  { value: "test", label: "test" },
+  { value: "chore", label: "chore" }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: null,
+      scope: "",
+      subject: "",
+      body: "",
+      footer: ""
+    };
+  }
+
+  getCommitMessage = () => {
+    let tmpString = `${this.state.type ? this.state.type.value : ""} (${
+      this.state.scope
+    }): ${this.state.subject} 
+
+${this.state.body}
+    
+${this.state.footer}`;
+
+    return tmpString;
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value ? e.target.value : ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="app-container">
+          <div className="two-screens-container">
+            <div className="screen left">
+              <Form>
+                <FormGroup>
+                  <Label>Type</Label>
+                  <Select
+                    name="type"
+                    value={this.state.type}
+                    onChange={e => {
+                      this.setState({ type: e ? e : null });
+                    }}
+                    options={options}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Scope</Label>
+                  <Input
+                    onChange={this.onChange}
+                    value={this.state.scope}
+                    name={"scope"}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Subject</Label>
+                  <Input
+                    onChange={this.onChange}
+                    value={this.state.subject}
+                    name={"subject"}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Message Body</Label>
+                  <Input
+                    onChange={this.onChange}
+                    value={this.state.body}
+                    name={"body"}
+                    type="textarea"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Footer</Label>
+                  <Input
+                    onChange={this.onChange}
+                    value={this.state.footer}
+                    name={"footer"}
+                    type="textarea"
+                  />
+                </FormGroup>
+              </Form>
+            </div>
+            <div className="screen right">
+              <div
+                className="copy-button"
+                onClick={() => {
+                  var el = document.createElement("textarea");
+                  // Set value (string to be copied)
+                  el.value = '"' + this.getCommitMessage() + '"';
+                  // Set non-editable to avoid focus and move outside of view
+                  el.setAttribute("readonly", "");
+                  el.style = { position: "absolute", left: "-9999px" };
+                  document.body.appendChild(el);
+                  // Select text inside element
+                  el.select();
+                  // Copy text to clipboard
+                  document.execCommand("copy");
+                  // Remove temporary element
+                  document.body.removeChild(el);
+                }}>
+                <FontAwesomeIcon icon={faClipboard} />
+              </div>
+              <div id="commit-message-formatted"> Commit message </div>
+              <div className="commit-message-formatted">
+                {this.getCommitMessage()}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
