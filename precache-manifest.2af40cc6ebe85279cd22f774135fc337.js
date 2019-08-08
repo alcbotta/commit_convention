0990e99adfbd5a81dc95ccada9998585ee2ae691
Andre Botta <andre.botta@outcomex.com.au>
2019-08-08 14:25:53 +1000

    refactor (App.js): remove animated copy to clipboard button

---
+++ precache-manifest.2af40cc6ebe85279cd22f774135fc337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd661772c1bda1100153cf0b7880246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb5e0ddc3b5d4ae84f9",
+    "url": "/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "ca741a5646c04fdac584",
+    "url": "/static/css/main.9b6d829c.chunk.css"
+  },
+  {
+    "revision": "5eb5e0ddc3b5d4ae84f9",
+    "url": "/static/js/2.5dac8f47.chunk.js"
+  },
+  {
+    "revision": "ca741a5646c04fdac584",
+    "url": "/static/js/main.02d62fe3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
