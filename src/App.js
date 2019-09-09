ff788b40ba2ac1e03e21d55af6b5d59c62fe8e49
Andre Botta <andre.botta@outcomex.com.au>
2019-09-09 15:00:59 +1000

    test

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        test2
+        test1
         <div className="app-container">
           <div className="two-screens-container">
             <div className="screen left">
